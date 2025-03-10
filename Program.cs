7a75da141f1a0bf9d787ba18f59076c9dcf60a98
Samire <xsamire2@gmail.com>
2025-03-10 23:40:32 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,105 @@
+﻿
+//1------
+
+using System.ComponentModel.Design;
+
+//1----
+
+//Console.WriteLine("Enter a number:");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number < 10 || number > 99)  
+//{
+//    Console.WriteLine("Xeta ancaq 2 regemli eded daxil ede bilersen");
+//}
+//else  
+//{
+//    int tensPlace = number / 10;  // Onluq hissə
+//    int onesPlace = number % 10;  // Təklik hissə
+//    if (tensPlace > onesPlace)
+//    {
+//        Console.WriteLine(tensPlace);
+//    }
+//    else if (tensPlace == onesPlace)
+//    {
+//        Console.WriteLine("Equal");
+//    }
+//    else
+//    {
+//        Console.WriteLine(onesPlace);
+//    }
+//}
+
+
+// 1 ci misalin diger usulu------------------
+
+//int number = 45;
+//int tensPlace = number / 10;
+//int onesPlace = number % 10;
+//if (tensPlace > onesPlace)
+//{
+//    Console.WriteLine(tensPlace);
+//}
+//else if (tensPlace == onesPlace)
+//{
+//    Console.WriteLine("They are equal");
+//}
+//else
+//{
+//    Console.WriteLine(onesPlace);
+//}
+
+
+
+
+//2--------
+
+//Console.WriteLine("ededi daxil et:");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int sum = 0;
+//if (number >= 100 && number <= 999)
+//{
+//    int hundredsPlace = number / 100;
+//    int tensPlace = number % 100 / 10;
+//    int onesPlace = number % 10;
+//    sum = hundredsPlace + tensPlace + onesPlace;
+//    Console.WriteLine(sum);
+//}
+//else
+//{
+//    Console.WriteLine("Xeta ancaq 3 reqemli eded daxil ede bilersen");
+//}
+
+
+
+//3
+
+//Console.WriteLine("Simvol daxil et:");
+//char choice = Convert.ToChar(Console.ReadLine());
+
+//switch (choice)
+//{
+//    case 'a':
+//        Console.WriteLine("azerbaycan dili secildi");
+//        break;
+//    case 'r':
+//        Console.WriteLine("rus dili secildi");
+//        break;
+//    case 'A':
+//        Console.WriteLine("AZ");
+//        break;
+//    case 'R':
+//        Console.WriteLine("RU");
+//        break;
+//    default:
+//        Console.WriteLine("Yanlis sorgu");
+//        break;
+//}
+
+
+
+
+
+
+
+
