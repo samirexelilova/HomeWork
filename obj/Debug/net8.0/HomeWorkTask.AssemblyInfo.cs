6a6a53628727848f8ebfb96ae331e13611f5722b
Samire <xsamire2@gmail.com>
2025-03-11 15:07:39 +0400

    add Flowcharts

--- obj/Debug/net8.0/HomeWorkTask.AssemblyInfo.cs
+++ obj/Debug/net8.0/HomeWorkTask.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HomeWorkTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a75da141f1a0bf9d787ba18f59076c9dcf60a98")]
 [assembly: System.Reflection.AssemblyProductAttribute("HomeWorkTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HomeWorkTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
